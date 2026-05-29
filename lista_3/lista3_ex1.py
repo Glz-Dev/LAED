d3dabcc1384d880b15db5582e4d3e2527d2a5d25
Glz-Dev <geraldolzDev@gmail.com>
2026-05-29 10:02:18 -0300

    LISTA DE EXERCICIOS 03

---
+++ lista_3/lista3_ex1.py
@@ -0,0 +1,9 @@
+# O MELHOR CASO - LETRA A
+'''
+ O melhor caso se da quando a lista já se encontra quase toda ordenada, nesse caso o tempo de execução seria O(n)
+'''
+# O PIOR CASO - LETRA B
+'''
+O pior caso seria quando para cada elemento ele precisa percorrer quase toda a lista, sendo o seu tempo O(n²)
+'''
+
